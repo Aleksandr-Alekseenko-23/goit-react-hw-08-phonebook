c9204436486d6fb758b8cf0bae88fab19f4a5736
Aleksandr-Alekseenko-23 <alekseenko48@bigmir.net>
2022-11-07 02:52:37 +0200

    Routes

--- src/components/Routes/Privat/PrivateRoute.jsx
+++ src/components/Routes/Privat/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { getIsLoggetIn } from '../../redux/Auth/AuthSelectors';
+import { getIsLoggetIn } from '../../../redux/Auth/AuthSelectors';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
